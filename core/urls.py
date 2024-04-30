00ac523778b29cdc3c08c958e6c27f03b1bf4f3a
Jitesh Chauhan <Jiteshckk@users.noreply.github.com>
2024-04-30 23:54:22 +0100

    Third Try

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     
-    
+    path(" ",recipes, name="recipes"),
     
     path('admin/', admin.site.urls),
     path("login_page/",login_page, name="login_page"),
