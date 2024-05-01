96f2cc063dc07c138355cfcd0cc4f4a8ecff93bf
Jitesh Chauhan <Jiteshckk@users.noreply.github.com>
2024-05-01 01:20:36 +0100

    final

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -97,7 +97,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
