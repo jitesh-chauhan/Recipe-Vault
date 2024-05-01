76c340d53c8a7fa15dd044469864c455f60964d9
Jitesh Chauhan <Jiteshckk@users.noreply.github.com>
2024-05-01 01:02:43 +0100

    final

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'C:/Users/VaRdaiN/Desktop/Django/core',
     }
 }
 
