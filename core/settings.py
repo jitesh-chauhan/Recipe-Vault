376a826946176faaeb3d3587166836fd67c26aef
Jitesh Chauhan <Jiteshckk@users.noreply.github.com>
2024-05-02 17:44:28 +0100

    new 1

--- core/settings.py
+++ core/settings.py
@@ -187,18 +187,18 @@ STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_root')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static','staticfiles_build'),
+]
 
 
-# MEDIA_ROOT = '/media/'
+MEDIA_ROOT = '/media/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS=(
-#         os.path.join(BASE_DIR,'static'),
-# )
+STATICFILES_DIRS=(
+        os.path.join(BASE_DIR,'static','staticfiles_build'),
+)
 
 
  
